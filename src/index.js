2f570edd23457adef3a202cf4953046db851b011
kumailnxtdev <komailnaqvi616@gmail.com>
2023-07-20 14:54:27 +0500

    api basics and expressjs init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const app = require('./server')
+
+app.listen(3001, ()=>{
+    console.log("hello  on the http://localhost:3001git ");
+})
+
+
+// const http = require("http");
+
+// const server= http.createServer((req, res)=>{
+//     if (req.method === 'GET' && req.url === '/'){
+//         console.log("Hello From server");
+//         res.end();
+//     }
+// })
+
+
+// server.listen(3001,() =>{
+//     console.log('server on http://localhost:3001');
+// });
+
