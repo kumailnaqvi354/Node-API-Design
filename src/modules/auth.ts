289ae8cc068886913e5494fb6dda7f0b1abeb93e
kumailnxtdev <komailnaqvi616@gmail.com>
2023-07-24 18:51:52 +0500

    JWT module

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,7 +1,9 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-
-export const createJWT = (user) =>{
-    
-const token = jwt.sign({id: user.id, username: user.username}, )
-}
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;git add
+};
