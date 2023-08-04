9f35ca497e07a3c7bf860419648934061207e3e2
kumailnxtdev <komailnaqvi616@gmail.com>
2023-08-04 17:20:16 +0500

    handler/update.ts added

---
+++ src/handlers/update.ts
@@ -0,0 +1,7 @@
+import prisma from "../db"
+
+export const getOneUpdate = async (req, res) =>{}
+export const getUpdates = async (req, res) =>{}
+export const createUpdate = async (req, res) =>{}
+export const updateUpdate = async (req, res) =>{}
+export const deleteUpdate = async (req, res) =>{}
