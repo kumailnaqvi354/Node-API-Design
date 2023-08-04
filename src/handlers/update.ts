1e20a29ff3bf42330539ea2ab39459f342b3535b
kumailnxtdev <komailnaqvi616@gmail.com>
2023-08-04 17:37:01 +0500

    handler/update.ts: getOneUpdate and getUpdate handler added

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -1,7 +1,32 @@
 import prisma from "../db"
 
-export const getOneUpdate = async (req, res) =>{}
-export const getUpdates = async (req, res) =>{}
+export const getOneUpdate = async (req, res) =>{
+    const update = await prisma.update.findUnique({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.json({data: update})
+}
+export const getUpdates = async (req, res) =>{
+
+    const products = await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.id
+        },
+        include:{
+            updates:true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, products) =>{
+            return [...allUpdates, ...products.updates]
+    },[])
+
+    res.json({data:updates})
+
+}
 export const createUpdate = async (req, res) =>{}
 export const updateUpdate = async (req, res) =>{}
 export const deleteUpdate = async (req, res) =>{}
