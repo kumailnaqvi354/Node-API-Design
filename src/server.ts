64590a4603211561b3bc6199d19fec53ade3da76
kumailnxtdev <komailnaqvi616@gmail.com>
2023-08-18 19:17:20 +0500

    error logs update

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res, next) => {
   setTimeout(() => {
-    next( new Error(' hello'));
+    next(new Error(" hello"));
   }, 1);
 });
 
@@ -25,10 +25,17 @@ app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signIn);
 
-app.use((err, req, res, next)=>{
-  console.log("Err", err);
-  res.json({message:'oops there was an error'});
-  
-})
+app.use((err, req, res, next) => {
+  if (err.type == "auth") {
+    res.status(401);
+    res.json({ message: "unauthorized" });
+  } else if (err.type == "input") {
+    res.status(400);
+    res.json({ message: "Invalid input " });
+  } else {
+    res.status(500);
+    res.json({ message: "oops, thats on our end" });
+  }
+});
 
 export default app;
